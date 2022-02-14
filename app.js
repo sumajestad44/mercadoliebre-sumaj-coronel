005cdc4ba0c66ec831eda750bfc90e9ab335b239
unknown <sumajcoronel2002@gmail.com>
2022-02-14 09:15:50 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicFolderPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicFolderPath));
+
+app.listen(process.env.PORT || 3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
